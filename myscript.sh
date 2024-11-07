9a92697aafc69dbcfcdd1912ed4b1c857d6d9048
Ravishan <148760770+ravishansenevirathna@users.noreply.github.com>
2024-11-07 20:10:19 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -19,3 +19,50 @@ echo "Your username is : $USER."
 #note that --> the env variables are write dows as upper case.Also you can find the env variables that pressing -->
 
 env
+
+# the astrict  "*" is a wildcard in hthe bash.
+
+
+#########################################################################################################################
+#if statement
+
+mynum=300
+
+if [ $mynum -eq 200 ]
+then
+        echo "the condition is true."
+else
+        echo "the condition is false"
+fi
+
+if [ $mynum -ne 200 ]
+then
+        echo "the second condition is true."
+else
+        echo "the second condition is false"
+fi
+
+##################################################################
+#check if the specific file is present in the file system.
+
+if [ -f ~/myLife ]
+then
+        echo "your file is found"
+else
+        echo "your file is not found"
+fi
+
+
+#################################################################
+#install htop, if the file system does not have the htop
+
+
+command=/usr/bin/htop
+
+if [ -f $command ]
+then
+        echo "htop is available lets run it"
+else
+        echo "htop is not available installing it"
+        sudo apt update && sudo apt install -y htop
+fi
