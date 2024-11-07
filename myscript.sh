ad102ba1374884593d070b14ed8ca5f3efa59df9
Ravishan <148760770+ravishansenevirathna@users.noreply.github.com>
2024-11-07 18:35:38 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,1 +1,21 @@
-ls -la
+#!/bin/bash
+
+myname="ravishan"
+myage="30"
+
+echo "Hello my name is $myname."
+echo "Im $myage years old."
+
+nowDate=$(date)
+echo "current date is: $nowDate."
+
+#insted the data is getting like above you can get the date time like this.just write date.
+date
+
+echo "Your username is : $USER."
+
+#this kind of variables are already defined.
+#the best practices is --> keep your variables are lower case.
+#note that --> the env variables are write dows as upper case.Also you can find the env variables that pressing -->
+
+env
